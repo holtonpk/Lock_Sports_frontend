65732820afa5e71b08dd8cc2c2078b7c31636007
Patrick Holton <holtonpk@gmail.com>
2022-09-04 21:06:29 -0600

    poolPage

--- src/components/PickMenu.js
+++ src/components/PickMenu.js
@@ -104,89 +104,91 @@ const PickMenu = ({
         />
       </div>
       <div className="flex flex-col    w-full    rounded-[10px]  h-fit mt-4 relative max-w-[1500px] bg-white02">
-        <div className="flex flex-col items-center justify-between w-[90%] mx-auto lg:flex-row md:mb-4 ">
-          <div className="flex flex-row items-center justify-between w-full">
-            <button
-              className="block md:hidden"
-              id="weekViewMinus"
-              onClick={() => WeekSelector(-1)}
-            >
-              <BiRightArrow className="w-6 h-6 rotate-180 fill-c5" />
-            </button>
-            <h1 className="text-c1 text-7xl font-bold mr-4 whitespace-nowrap mb-1 w-[fit-content]  ">
-              {"Week " + weekView}
-            </h1>
-            <button
-              className="block md:hidden"
-              id="weekViewPlus"
-              onClick={() => WeekSelector(1)}
-            >
-              <BiRightArrow className="w-6 h-6 fill-c5" />
-            </button>
-          </div>
-          {(() => {
-            if (thisWeeksPrediction) {
-              let Team = getTeamData(thisWeeksPrediction.selectedWinner);
-              return (
-                <div className="flex flex-row items-center w-fit">
-                  <div className="text-base font-bold text-white sm:text-lg md:text-2xl whitespace-nowrap">
-                    Your Pick :
-                  </div>
+        <div className="flex flex-row items-center w-full">
+          <div className="flex flex-col items-center justify-between w-[90%] mx-auto lg:flex-row md:mb-4 ">
+            <div className="flex flex-row items-center justify-between w-full ">
+              <button
+                className="block md:hidden"
+                id="weekViewMinus"
+                onClick={() => WeekSelector(-1)}
+              >
+                <BiRightArrow className="w-10 h-10 rotate-180 fill-c5 " />
+              </button>
+              <h1 className="text-c1 text-7xl font-bold mr-4 whitespace-nowrap mb-1 w-[fit-content]  ">
+                {"Week " + weekView}
+              </h1>
+              <button
+                className="block md:hidden"
+                id="weekViewPlus"
+                onClick={() => WeekSelector(1)}
+              >
+                <BiRightArrow className="w-10 h-10 fill-c5" />
+              </button>
+            </div>
+            {(() => {
+              if (thisWeeksPrediction) {
+                let Team = getTeamData(thisWeeksPrediction.selectedWinner);
+                return (
+                  <div className="flex flex-row items-center w-fit">
+                    <div className="text-base font-bold text-white sm:text-lg md:text-2xl whitespace-nowrap">
+                      Your Pick :
+                    </div>
 
-                  <div
-                    className={
-                      "flex flex-row items-center   px-4  rounded-[10px] w-[fit-content]  py-2  box-border  "
-                    }
-                  >
-                    <div className="flex flex-row mx-auto">
-                      <img
-                        src={Team.WikipediaLogoUrl}
-                        alt="no logo"
-                        className="w-8 h-8 mr-4 md:w-14 md:h-14"
-                      />
+                    <div
+                      className={
+                        "flex flex-row items-center   px-4  rounded-[10px] w-[fit-content]  py-2  box-border  "
+                      }
+                    >
+                      <div className="flex flex-row mx-auto">
+                        <img
+                          src={Team.WikipediaLogoUrl}
+                          alt="no logo"
+                          className="w-8 h-8 mr-4 md:w-14 md:h-14"
+                        />
 
-                      <div className="flex flex-row items-center">
-                        <h1 className="mr-1 text-base sm:text-2xl whitespace-nowrap text-c1 ">
-                          {Team.City}
-                        </h1>
-                        <h1 className="mr-2 text-base font-bold sm:text-2xl text-c1 ">
-                          {Team.Name}
-                        </h1>
-                        {/* <FaLock className="w-5 h-5 fill-c1" /> */}
+                        <div className="flex flex-row items-center">
+                          <h1 className="mr-1 text-base sm:text-2xl whitespace-nowrap text-c1 ">
+                            {Team.City}
+                          </h1>
+                          <h1 className="mr-2 text-base font-bold sm:text-2xl text-c1 ">
+                            {Team.Name}
+                          </h1>
+                          {/* <FaLock className="w-5 h-5 fill-c1" /> */}
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            } else {
-              return (
-                <>
-                  <div className="flex w-[fit-content] xl:mr-6 flex-row mt-2  items-center  text-c5  border-c5 border-2 bg-c5_20 rounded-b-[5px]  px-2  top-0 mx-auto md:left-[50%]">
-                    <h1 className="mr-2 text-[16px] md:text-xl whitespace-nowrap">
-                      Pick deadline
-                    </h1>
-                    <h1 className=" rounded-[5px]  whitespace-nowrap text-[16px] md:text-xl px-1">
-                      {date.toLocaleString("en-US", {
-                        day: "numeric",
-                        month: "numeric",
-                        hour: "numeric",
-                        minute: "numeric",
-                      }) + " ET"}
-                    </h1>
-                  </div>
-
-                  <div className="flex flex-row items-center w-full my-4">
-                    <div className="relative p-4 mr-2 border-2 rounded-full bg-blue-20 border-c1">
-                      <BsFillHandIndexFill className="absolute w-4 h-4 -translate-x-1/2 -translate-y-1/2 fill-c1 left-1/2 top-1/2" />
+                );
+              } else {
+                return (
+                  <>
+                    <div className="flex w-[fit-content] xl:mr-6 flex-row mt-2  items-center  text-c5  border-c5 border-2 bg-c5_20 rounded-b-[5px]  px-2  top-0 mx-auto md:left-[50%]">
+                      <h1 className="mr-2 text-[16px] md:text-xl whitespace-nowrap">
+                        Pick deadline
+                      </h1>
+                      <h1 className=" rounded-[5px]  whitespace-nowrap text-[16px] md:text-xl px-1">
+                        {date.toLocaleString("en-US", {
+                          day: "numeric",
+                          month: "numeric",
+                          hour: "numeric",
+                          minute: "numeric",
+                        }) + " ET"}
+                      </h1>
                     </div>
-                    <h1 className="text-2xl font-thin text-white whitespace-nowrap">
-                      Pick a Team Below
-                    </h1>
-                  </div>
-                </>
-              );
-            }
-          })()}
+
+                    <div className="flex flex-row items-center w-full my-4">
+                      <div className="relative p-4 mr-2 border-2 rounded-full bg-blue-20 border-c1">
+                        <BsFillHandIndexFill className="absolute w-4 h-4 -translate-x-1/2 -translate-y-1/2 fill-c1 left-1/2 top-1/2" />
+                      </div>
+                      <h1 className="text-2xl font-thin text-white whitespace-nowrap">
+                        Pick a Team Below
+                      </h1>
+                    </div>
+                  </>
+                );
+              }
+            })()}
+          </div>
         </div>
         <div className="w-[100%] border-t-[.5px] border-c1"></div>
         <div className="w-full px-3">
